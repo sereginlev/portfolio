8f9472b0cd284c6557f5f2c3b8f37b326e0b10d4
Lev Seregin <sereginlev1437@gmail.com>
2023-06-02 16:02:30 +0200

    some fixes

--- src/components/Main/About.jsx
+++ src/components/Main/About.jsx
@@ -29,7 +29,7 @@ function About() {
 
 			<p className={styles.textBold}>I can't draw, but I've always wanted to learn. Front-end development allows me to do this to some extent. I also like to see the results of my work, which is why I chose the front-end direction.</p>
 
-			<h3 className={styles.subtitle}>Some more information</h3>
+			<h3 className={styles.subtitle}>More information</h3>
 			<p className={styles.text}>I was born and raised in Volgograd, Russia. Received an engineering education and worked in construction for a long time. At some point, I realized that changes were needed and decided to change my profession.
 			<br/>
 			<br/>
