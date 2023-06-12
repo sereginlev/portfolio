d90be21f9dc86b59ac275d0c4e16cfbb128fc9ce
Lev Seregin <sereginlev1437@gmail.com>
2023-06-12 16:26:18 +0200

    deleted animations from all blocks.

--- src/components/Main/About.jsx
+++ src/components/Main/About.jsx
@@ -1,30 +1,11 @@
 import React from 'react';
-import { motion } from 'framer-motion';
 
 import styles from 'scss/components/Main/About.module.scss';
-
-const textAnimation = {
-	hidden: {
-		y: 100,
-		opacity: 0
-	},
-	visible: {
-		y: 0,
-		opacity: 1,
-		transition: { property: 'all', duration: 0.5 }
-	}
-}
 
 function About() {
 
 	return (
-		<motion.section
-			initial='hidden'
-			whileInView='visible'
-			viewport={{ amount: 0.2, once: true }}
-			variants={textAnimation}
-			className={styles.about}
-		>
+		<section className={styles.about}>
 			<h2 className={styles.title}>About me.</h2>
 
 			<p className={styles.textBold}>I can't draw, but I've always wanted to learn. Front-end development allows me to do this to some extent. I also like to see the results of my work, which is why I chose the front-end direction.</p>
@@ -35,7 +16,7 @@ function About() {
 			<br/>
 			If you want to know something personal about me, then I adore basketball, fishing and 'The Sopranos' series.
 			</p>
-		</motion.section>
+		</section>
 	)
 }
 
