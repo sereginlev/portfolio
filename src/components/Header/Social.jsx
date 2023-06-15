d31cb99d563dfe4b6c2d39c91c09e4c99b4cd696
Lev Seregin <sereginlev1437@gmail.com>
2023-06-15 12:35:58 +0200

    added link for resume and changed url to linkedin

--- src/components/Header/Social.jsx
+++ src/components/Header/Social.jsx
@@ -6,10 +6,10 @@ function Social() {
 	return (
 		<article className={styles.social}>
 			<p className={styles.text}>
-				Find me at <a className={styles.link} href='https://github.com/sereginlev' target='_blank'>GitHub</a>, <a className={styles.link} href='https://t.me/sereginlev' target='_blank'>Telegram</a> and <a className={styles.link} href='https://www.linkedin.com/in/sereginlev/' target='_blank'>LinkedIn</a>.
+				Find me at <a className={styles.link} href='https://github.com/sereginlev' target='_blank'>GitHub</a>, <a className={styles.link} href='https://t.me/sereginlev' target='_blank'>Telegram</a> and <a className={styles.link} href='https://www.linkedin.com/in/levseregin/' target='_blank'>LinkedIn</a>.
 			</p>
 			<p className={styles.text}>
-				Download <a className={styles.link} href='#' target='_blank'>my resume</a> (PDF 469 kb)
+				Download <a className={styles.link} href='https://drive.google.com/file/d/1WzAwP9IgFfsVlu7QAA9y18NMezKl6sWO/view' target='_blank'>my resume</a> (PDF 718 kb)
 			</p>
 		</article>
 	)
